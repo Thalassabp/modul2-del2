6cea6b8584323942d4a2c933a40dc8977c262c19
Thalassa <Thalassabp@hotmail.com>
2016-09-28 11:10:28 +0200

    Oprettelse af dokumenter

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    
+    <title>Welcome to your website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+   <h1>Please log in or register</h1> 
+</body>
+</html>
